0048b91ee04373f9343cd30b11f65cdfafc2d13a
Luzaks <kanemekanik@gmail.com>
2020-03-19 17:01:48 -0600

    Feat: Add remove method for books and work in general logic for update status

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 let myLibrary = [];
 
-function Book(title, author, genre, pages, readStatus) {
+function Book(book_id, title, author, genre, pages, readed, notReaded, reading) {
+    this.book_id = book_id;
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.pages = pages;
-    this.readStatus = readStatus;
+    this.readed = readed;
+    this.notReaded = notReaded;
+    this.reading = reading;
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(
         new Book(
+            myLibrary.length + 1,
             document.getElementById('title').value,
             document.getElementById('author').value,
             document.getElementById('pages').value,
             document.getElementById('genre').value,
-            document.getElementById('readStatus').value,
+            document.getElementById('readStatus').checked,
+            document.getElementById('readStatus1').checked,
+            document.getElementById('readStatus2').checked,
         )
     );
-
     render();
 }
 
@@ -30,13 +35,21 @@ function cleanForm() {
     document.getElementById('readStatus').value = "";
 }
 
-function showForm() {
-    document.getElementById('addBook').style.display = 'block';
+function showForm(formCreate) {
+    formCreate ? document.getElementById('addBook').style.display = 'block' : document.getElementById('change-status-form').style.display = 'block';
 }
 
-function hideForm() {
-    document.getElementById('addBook').style.display = 'none';
-    cleanForm();
+function hideForm(formCreate) {
+    formCreate ? document.getElementById('addBook').style.display = 'none' : document.getElementById('change-status-form').style.display = 'none';
+}
+
+function updateStatus(check) {
+    const selected_book = myLibrary.find((elem) => elem.id === check);
+    selected_book.readed = document.getElementById('readStatus3').checked;
+    selected_book.notReaded = document.getElementById('readStatus4').checked;
+    selected_book.reading = document.getElementById('readStatus5').checked;
+    render();
+    console.log(selected_book);
 }
 
 function render() {
@@ -48,16 +61,26 @@ function render() {
                             <td>${item.author}</td>
                             <td>${item.genre}</td>
                             <td>${item.pages}</td>
-                            <td>${item.readStatus}</td>
+                            <td><a href="#" class="read-status-link" onclick="showForm(false)" id="status-${item.book_id}"> ${item.readed ? item.readed : item.notReaded ? item.notReaded : item.reading}</a></td>
+                            <td>
+                                <img class="remove-book-info" onclick="removeBook(${item.book_id})" id="remove-book-btn-${item.book_id}" style="width: 30px; height: 30px; cursor: pointer" src="https://img.icons8.com/plasticine/100/000000/filled-trash.png" alt="remove-button"/>
+                            </td>
                         </tr>
                        </tbody>`;
-        });
+    });
     document.getElementById('books-info').innerHTML = booksInfo;
+}
+
+function removeBook(id) {
+    const element = myLibrary.find((elem) => elem.book_id === id);
+    myLibrary.splice(myLibrary.indexOf(element), 1);
+    render();
 }
 
 function defaultBooks() {
     myLibrary.push(
         new Book(
+            0,
             'Silent Hill',
             'Team Silent',
             'Games',
@@ -66,6 +89,7 @@ function defaultBooks() {
         ));
     myLibrary.push(
         new Book(
+            1,
             'Resident Evil',
             'Jill Valentine',
             'Games',
@@ -74,6 +98,7 @@ function defaultBooks() {
         ));
     myLibrary.push(
         new Book(
+            2,
             'Un homme qui dort',
             'Georges Perec',
             'Novel',
